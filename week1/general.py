385db4e7b2b8aa9d3bce475a694c6e56191c1b1f
jeklen <zhql0907@outlook.com>
2017-07-18 11:54:43 +0800

    add week1

---
+++ week1/general.py
@@ -0,0 +1,15 @@
+import requests
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "产生异常"
+
+if __name__ == "__main__":
+    url = "http://www.baidu.com"
+    text = getHTMLText(url)
+    print(getHTMLText(url))
